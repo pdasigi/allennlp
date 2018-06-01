53d38089bc8cf63ead6c034a7a527cd3c091e4a8
Matt Gardner <mattg@allenai.org>
2018-06-01 16:23:45 -0700

    Added back in action bias

--- allennlp/models/semantic_parsing/wikitables/wikitables_mml_semantic_parser.py
+++ allennlp/models/semantic_parsing/wikitables/wikitables_mml_semantic_parser.py
@@ -48,6 +48,9 @@ class WikiTablesMmlSemanticParser(WikiTablesSemanticParser):
     input_attention : ``Attention``
         We compute an attention over the input question at each step of the decoder, using the
         decoder hidden state as the query.  Passed to WikiTablesDecoderStep.
+    add_action_bias : ``bool``, optional (default=True)
+        If ``True``, we will learn a bias weight for each action that gets used when predicting
+        that action, in addition to its embedding.  Passed to super class.
     training_beam_size : ``int``, optional (default=None)
         If given, we will use a constrained beam search of this size during training, so that we
         use only the top ``training_beam_size`` action sequences according to the model in the MML
@@ -85,6 +88,7 @@ class WikiTablesMmlSemanticParser(WikiTablesSemanticParser):
                  decoder_beam_search: BeamSearch,
                  max_decoding_steps: int,
                  input_attention: Attention,
+                 add_action_bias: bool = True,
                  training_beam_size: int = None,
                  use_neighbor_similarity_for_linking: bool = False,
                  dropout: float = 0.0,
@@ -98,6 +102,7 @@ class WikiTablesMmlSemanticParser(WikiTablesSemanticParser):
                          encoder=encoder,
                          entity_encoder=entity_encoder,
                          max_decoding_steps=max_decoding_steps,
+                         add_action_bias=add_action_bias,
                          use_neighbor_similarity_for_linking=use_similarity,
                          dropout=dropout,
                          num_linking_features=num_linking_features,
@@ -109,6 +114,8 @@ class WikiTablesMmlSemanticParser(WikiTablesSemanticParser):
                                                    action_embedding_dim=action_embedding_dim,
                                                    input_attention=input_attention,
                                                    num_start_types=self._num_start_types,
+                                                   predict_start_type_separately=True,
+                                                   add_action_bias=self._add_action_bias,
                                                    mixture_feedforward=mixture_feedforward,
                                                    dropout=dropout)
 
